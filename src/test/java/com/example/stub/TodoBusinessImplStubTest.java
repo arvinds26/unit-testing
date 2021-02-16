108b2730304019806d31e882a2bc8f8dd9a564f2
unknown <arvindsrinivas26@gmail.com>
2021-02-16 19:51:24 +0530

    stub service

--- src/test/java/com/example/stub/TodoBusinessImplStubTest.java
+++ src/test/java/com/example/stub/TodoBusinessImplStubTest.java
@@ -1,2 +1,20 @@
-package com.example.stub;public class TodoBusinessImplStubTest {
+package com.example.stub;
+
+import com.example.service.TodoService;
+import com.example.serviceimpl.TodoBusinessImpl;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class TodoBusinessImplStubTest {
+
+  @Test
+  public void usingAStub() {
+    TodoService todoService = new TodoServiceStub();
+    TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+    List<String> todos = todoBusinessImpl.retrieveTodosRelatedToSpring("Spring");
+    assertEquals(2, todos.size());
+  }
 }
--- src/test/java/com/example/stub/TodoServiceStub.java
+++ src/test/java/com/example/stub/TodoServiceStub.java
@@ -1,2 +1,14 @@
-package com.example.stub;public class TodoServiceStub {
+package com.example.stub;
+
+import com.example.service.TodoService;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TodoServiceStub implements TodoService {
+  public List<String> retrieveTodos(String user) {
+    return Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
+  }
+
+  public void deleteTodo(String todo) {}
 }
